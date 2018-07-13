91b5f2fcec4d9b0bb869420484c0c4d6c1d68ba2
106-ПК\106 <dmitro.pf@gmail.com>
2018-07-13 12:41:23 +0300

    no message

---
+++ keypad4x4.c
@@ -0,0 +1,224 @@
+﻿//keypad4x4.c
+
+#include "keypad4x4.h"
+//**********************************************************************************
+//переменные в которых хранится текущие состояния
+static volatile STATE_column stateColumn = column1;
+static volatile STATE_key_out stateKeypad = key_out_state_NULL;
+
+//**********************************************************************************
+void keypad_init(void)
+{
+	KEYPAD_GPIO_RCC;
+	COLUMNS_init;
+	ROWS_init;
+	COLUMN_init_SET;
+}
+
+//**********************************************************************************
+//состояния системы
+STATE_column getStateColumn(void);//получить текущий опрашиваемый столбец
+{
+	return stateColumn;
+}
+void setStateColumn(STATE_column newState);//установить опрашиваемый столбец
+{
+	stateColumn = newState;
+}
+
+STATE_key_out getStateKeypad(void);//получить текущее состояние клавы
+{
+	return stateKeypad;
+}
+void setStateKeypad(STATE_key_out newState);//установить новое состояние клавы
+{
+	stateKeypad = newState;
+}
+
+/*******************************************************************************************
+функция реакции на нажатие кнопок клавы 
+!!!это надо запускать чаще чем опрос клавы, чтобы небыло пропусков в нажатии клавиш!!!
+********************************************************************************************/
+void dispatch_keypad(void)//диспетчер выполняющий задачи реагирующие на нажатие клавиш 
+{
+	switch (getStateKeypad())//
+	{
+		/****************************************************************/
+		case key_out_state_1://была нажата кн. 1
+		{
+			/****************************************************************/
+			//здесь ваш код, например:
+			LED_1_ON; 
+			/****************************************************************/
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_2://была нажата кн. 2
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_3://была нажата кн. 3
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_4://была нажата кн. 4
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_5://была нажата кн. 5
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_6://была нажата кн. 6
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_7://была нажата кн. 7
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_8://была нажата кн. 8
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_9://была нажата кн. 9
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_0://была нажата кн. 0
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_A://была нажата кн. A
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_B://была нажата кн. B
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_C://была нажата кн. C
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_D://была нажата кн. D
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_*://была нажата кн. *
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/
+		case key_out_state_#://была нажата кн. #
+		{
+			LED_1_OFF;					
+			setStateKeypad(key_out_state_NULL);
+			break;			
+		}
+		/****************************************************************/		
+	}
+}
+/*******************************************************************************************
+функция опроса клавы:
+- каждый вызов ф-ции опрашивает кнопки одного столбца. 
+- для опроса всей клавы нужно вызвать ф-цию 4-ре раза.
+- запускать в прерывании таймера с перидом 10-50 мс.
+********************************************************************************************/
+void get_keypad_push(void)//опрашиваем клавиатуру
+{
+	switch (getStateColumn())//переключаем режимы опроса столбцов
+	{
+		/****************************************************************/
+		case column1://опрашиваем строки 1-го столбца
+		{
+			COLUMN_1_SET;
+			if (ROW_1_IS_SET) 		{setStateKeypad(key_out_state_1);} //если нажатие в 1-й строке, то нажата кн - "1"
+			else if (ROW_2_IS_SET)	{setStateKeypad(key_out_state_4);} //если нажатие во 2-й строке, то нажата кн - "4"
+			else if (ROW_3_IS_SET)	{setStateKeypad(key_out_state_7);}
+			else if (ROW_4_IS_SET)	{setStateKeypad(key_out_state_*);}
+			else 					{setStateKeypad(key_out_state_NULL);}//если ничего не нажато			
+			setStateColumn(column2);
+			break;			
+		}
+		/****************************************************************/
+		case column2://опрашиваем строки 2-го столбца
+		{
+			COLUMN_2_SET;
+			if (ROW_1_IS_SET) 		{setStateKeypad(key_out_state_2);} //если нажатие в 1-й строке, то нажата кн - "2"
+			else if (ROW_2_IS_SET)	{setStateKeypad(key_out_state_5);} //если нажатие во 2-й строке, то нажата кн - "5"
+			else if (ROW_3_IS_SET)	{setStateKeypad(key_out_state_8);}
+			else if (ROW_4_IS_SET)	{setStateKeypad(key_out_state_0);}
+			else 					{setStateKeypad(key_out_state_NULL);}//если ничего не нажато			
+			setStateColumn(column3);
+			break;			
+		}
+		/****************************************************************/
+		case column3://опрашиваем строки 3-го столбца
+		{
+			COLUMN_3_SET;
+			if (ROW_1_IS_SET) 		{setStateKeypad(key_out_state_3);} //если нажатие в 1-й строке, то нажата кн - "3"
+			else if (ROW_2_IS_SET)	{setStateKeypad(key_out_state_6);} //если нажатие во 2-й строке, то нажата кн - "6"
+			else if (ROW_3_IS_SET)	{setStateKeypad(key_out_state_9);}
+			else if (ROW_4_IS_SET)	{setStateKeypad(key_out_state_#);}
+			else 					{setStateKeypad(key_out_state_NULL);}//если ничего не нажато			
+			setStateColumn(column4);
+			break;			
+		}
+		/****************************************************************/
+		case column4://опрашиваем строки 4-го столбца
+		{
+			COLUMN_4_SET;
+			if (ROW_1_IS_SET) 		{setStateKeypad(key_out_state_3);} //если нажатие в 1-й строке, то нажата кн - "A"
+			else if (ROW_2_IS_SET)	{setStateKeypad(key_out_state_6);} //если нажатие во 2-й строке, то нажата кн - "B"
+			else if (ROW_3_IS_SET)	{setStateKeypad(key_out_state_9);}
+			else if (ROW_4_IS_SET)	{setStateKeypad(key_out_state_#);}
+			else 					{setStateKeypad(key_out_state_NULL);}//если ничего не нажато			
+			setStateColumn(column1);
+			break;			
+		}		
+	}/****************************************************************/
+}
+//**********************************************************************************
---
+++ keypad4x4.h
@@ -0,0 +1,82 @@
+﻿//keypad4x4.h
+
+#ifndef __KEYPAD4X4_H__
+#define __KEYPAD4X4_H__
+
+#include <stm32f0xx.h>
+#include <system_stm32f0xx.h>
+#include "string.h" 
+#include "stdlib.h"
+
+//**********************************************************************************
+//инициализация GPIO работающей с клавиатурой
+#define KEYPAD_GPIO_RCC		RCC->AHBENR |= RCC_AHBENR_GPIOAEN //тактирование кнопок
+#define COLUMNS_init		GPIOA->MODER |= GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0 | GPIO_MODER_MODER3_0 //выход без подтяжки
+#define ROWS_init			GPIOA->MODER &= ~(GPIO_MODER_MODER4 | GPIO_MODER_MODER5 |GPIO_MODER_MODER6 | GPIO_MODER_MODER7) //вход без подтяжки
+
+//**********************************************************************************
+//установка выходов колонок
+#define COLUMN_1_SET 		GPIOA->BSRR |= GPIO_BSRR_BR_0 | GPIO_BSRR_BS_1 | GPIO_BSRR_BS_2 | GPIO_BSRR_BS_3 //0b 0111
+#define COLUMN_2_SET 		GPIOA->BSRR |= GPIO_BSRR_BS_0 | GPIO_BSRR_BR_1 | GPIO_BSRR_BS_2 | GPIO_BSRR_BS_3 //0b 1011
+#define COLUMN_3_SET 		GPIOA->BSRR |= GPIO_BSRR_BS_0 | GPIO_BSRR_BS_1 | GPIO_BSRR_BR_2 | GPIO_BSRR_BS_3 //0b 1101
+#define COLUMN_4_SET 		GPIOA->BSRR |= GPIO_BSRR_BS_0 | GPIO_BSRR_BS_1 | GPIO_BSRR_BS_2 | GPIO_BSRR_BR_3 //0b 1110
+#define COLUMN_init_SET 	GPIOA->BSRR |= GPIO_BSRR_BS_0 | GPIO_BSRR_BS_1 | GPIO_BSRR_BS_2 | GPIO_BSRR_BS_3 //0b 1111
+
+//**********************************************************************************
+//опрос выводов строк
+#define ROW_1_IS_SET 		((GPIOA->IDR & GPIO_IDR_4) == GPIO_IDR_4)
+#define ROW_2_IS_SET 		((GPIOA->IDR & GPIO_IDR_5) == GPIO_IDR_5)
+#define ROW_3_IS_SET 		((GPIOA->IDR & GPIO_IDR_6) == GPIO_IDR_6)
+#define ROW_4_IS_SET 		((GPIOA->IDR & GPIO_IDR_7) == GPIO_IDR_7)
+
+//**********************************************************************************
+//переменные в которых хранится текущие состояния
+extern static volatile STATE_column stateColumn;
+extern static volatile STATE_key_out stateKeypad;
+
+//**********************************************************************************
+//состояния
+typedef enum {
+	column1,	//опрос строк 1-й колонки
+	column2,	//опрос строк 2-й колонки
+	column3,	//опрос строк 3-й колонки
+	column4		//опрос строк 4-й колонки
+} STATE_column;
+
+typedef enum {
+	key_out_state_NULL,	//ничего не нажато
+	key_out_state_0,	//нажата кнопка 0
+	key_out_state_1,	//нажата кнопка 1
+	key_out_state_2,	//....
+	key_out_state_3,
+	key_out_state_4, 
+	key_out_state_5,
+	key_out_state_6,
+	key_out_state_7,
+	key_out_state_8,
+	key_out_state_9,
+	key_out_state_A,
+	key_out_state_B,
+	key_out_state_C,
+	key_out_state_D,
+	key_out_state_E,
+	key_out_state_F,
+	key_out_state_*,
+	key_out_state_#
+} STATE_key_out;
+
+//функции для работы с состояниями:
+STATE_column getStateColumn(void);//получить текущий опрашиваемый столбец
+void setStateColumn(STATE_column newState);//установить опрашиваемый столбец
+
+STATE_key_out getStateKeypad(void);//получить текущее состояние клавы
+void setStateKeypad(STATE_key_out newState);//установить новое состояние клавы
+
+//**********************************************************************************
+void keypad_init(void);//инициализация выводов под кнопки
+//**********************************************************************************
+void dispatch_keypad(void);//диспетчер выполняющий задачи реагирующие на нажатие клавиш 
+//**********************************************************************************
+void get_keypad_push(void);//опрашиваем клавиатуру
+//**********************************************************************************
+#endif /* __KEYPAD4X4_H_ */
